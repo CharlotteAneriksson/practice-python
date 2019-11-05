6f8d094df9339ad8c658812084dd87634e974103
Richarlotte <charlotteadolfssonolander@gmail.com>
2019-11-05 12:06:09 +0100

    question and answere done

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -7,13 +7,43 @@ def show_menu():
     return option
 
 
+def ask_questions():
+    questions = []
+    answers = []
+
+    with open('question.txt', 'r') as file:
+        lines = file.read().splitlines()
+
+    for i, text in enumerate(lines):
+        if i % 2 == 0:
+            questions.append(text)
+        else:
+            answers.append(text)
+
+    number_of_questions = len(questions)
+    questions_and_answers = zip(questions, answers)
+
+    score = 0
+
+    for question, answer in questions_and_answers:
+        guess = input(question + "> ")
+        if guess == answer:
+            score += 1
+            print("right!")
+            print(score)
+        else:
+            print("wrong!")
+
+    print("You got {0} correct out of {1}".format(score, number_of_questions))
+
+
 def add_question():
     print("")
-    question = input("Enter a question:\n ")
+    question = input("Enter a question:\n> ")
 
     print("")
     print("Ok then, tell me the answere")
-    answer = input("{0}\n ".format(question))
+    answer = input("{0}\n> ".format(question))
 
     file = open('question.txt', 'a')
     file.write(question + "\n")
@@ -28,7 +58,7 @@ def game_loop():
     while True:
         option = show_menu()
         if option == "1":
-            print("You selected 'Ask question'")
+            ask_questions()
         elif option == "2":
             add_question()
         elif option == "3":
